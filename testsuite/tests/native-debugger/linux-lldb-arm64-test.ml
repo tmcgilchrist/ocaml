bf8f94fb1f8310833f0c640d5fd4ca16236d3af0
Tim McGilchrist <timmcgil@gmail.com>
2024-07-09 10:47:13 +1000

    Linux LLDB ARM64 tests using Python scripting.

--- testsuite/tests/native-debugger/linux-lldb-arm64-test.ml
+++ testsuite/tests/native-debugger/linux-lldb-arm64-test.ml
@@ -10,7 +10,7 @@
    flags = "-g";
    all_modules = "meander.ml meander_c.c";
    ocamlopt.byte;
-   debugger_script = "${test_source_directory}/lldb-linux-script";
+   debugger_script = "${test_source_directory}/lldb-script";
    lldb;
    script = "sh ${test_source_directory}/sanitize.sh ${test_source_directory} \
    ${test_build_directory} ${ocamltest_response} linux-lldb-arm64-test";
