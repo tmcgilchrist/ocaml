59d787d234cb1793295040f31f0b7e51b1517590
Tim McGilchrist <timmcgil@gmail.com>
2026-03-17 08:40:14 +1100

    Exercise more assembly stubs in the native debugger tests.
    This should catch more CFI issues in the assembly stubs.

--- testsuite/tests/native-debugger/meander.ml
+++ testsuite/tests/native-debugger/meander.ml
@@ -1,3 +1,7 @@
+open Effect
+open Effect.Deep
+
+(* Test caml_c_call and caml_start_program: C-to-OCaml callback with exception *)
 external ocaml_to_c
          : unit -> int = "ocaml_to_c"
 exception E1
@@ -11,3 +15,37 @@ let omain () =
     with E2 -> 0
   with E1 -> 42
 let _ = assert (omain () = 42)
+
+(* Test caml_runstack: run function on a new fiber stack *)
+let[@inline never] on_fiber () =
+  Sys.opaque_identity 42
+
+let () =
+  let result = match_with on_fiber ()
+    { retc = Fun.id;
+      exnc = raise;
+      effc = (fun (type a) (_ : a t) -> None) }
+  in
+  assert (result = 42)
+
+(* Test caml_perform/caml_resume: effect handler perform and resume *)
+type _ Effect.t += Yield : unit Effect.t
+
+let[@inline never] on_perform () =
+  Sys.opaque_identity 99
+
+let[@inline never] performer () =
+  Effect.perform Yield;
+  on_perform ()
+
+let () =
+  let result = match_with performer ()
+    { retc = Fun.id;
+      exnc = raise;
+      effc = (fun (type a) (eff : a Effect.t) ->
+        match eff with
+        | Yield -> Some (fun (k : (a, _) continuation) ->
+            continue k ())
+        | _ -> None) }
+  in
+  assert (result = 99)
--- testsuite/tests/native-debugger/sanitize.awk
+++ testsuite/tests/native-debugger/sanitize.awk
@@ -50,6 +50,9 @@
 
     # Work around inconsistent name mangling
     gsub(/c_to_ocaml_[0-9]+/, "c_to_ocaml")
+    gsub(/on_fiber_[0-9]+/, "on_fiber")
+    gsub(/on_perform_[0-9]+/, "on_perform")
+    gsub(/performer_[0-9]+/, "performer")
 
     # Work around symbol versioning
     gsub(/__libc_start_main_impl$/, "__libc_start_mainXXXX")
